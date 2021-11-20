95bb4fe0249d0965d79e0b1985acff4ed5554682
Hank Kapka <hankkapka@gmail.com>
2021-11-20 15:34:44 -0600

    added logic to create Readme file and working on the shell of the readme file in generateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require("util");
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -44,12 +45,29 @@ const questions = [{
 
 
 
-inquirer.prompt(questions);
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName);
+        console.log(data);
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("you have created a ReadMe file");
+        }
+    })
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+
+
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown (data));    
+})
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -13,6 +15,16 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  https://github.com/${data.username}/${data.title}
+  ## Project Description:
+  ${data.description}
+  ### Table of Contents
+  - [Installation] (#installation)
+  - [Usage] (#usage)
+  - [License] (#license)
+  - [Questions] (#questions)
+
+  
 
 `;
 }
